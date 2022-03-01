57b7ebaacfd1c4f9fc151527999d4153ba99bc6f
Cyber Slug <lasminski@mail.ru>
2022-03-02 02:57:49 +0300

    feat: add prettier module

--- lib.js
+++ lib.js
@@ -1,43 +1,38 @@
-
-window.onresize = function() {
-    WIDTH = window.innerWidth;
-    HEIGHT = window.innerHeight;
-    console.log(`Current window size is ${WIDTH}x${HEIGHT}`);
-  }
-
+window.onresize = function () {
+    WIDTH = window.innerWidth
+    HEIGHT = window.innerHeight
+    console.log(`Current window size is ${WIDTH}x${HEIGHT}`)
+}
 
 export const setUrlParameter = (key, value) => {
-    const url = new URL(document.location.href);
-    url.searchParams.set(key, value);
-    document.location.assign(url.toString());
+    const url = new URL(document.location.href)
+    url.searchParams.set(key, value)
+    document.location.assign(url.toString())
 }
 
 export const setUrlHashParameter = (key, value) => {
-    const url = new URL(document.location.href);
+    const url = new URL(document.location.href)
     url.hash
 }
 
 export const createHisoryButton = () => {
-    const backButton = document.createElement("button");
-    const forwardButton = document.createElement("button");
-    backButton.appendChild(document.createTextNode("Back"));
-    forwardButton.appendChild(document.createTextNode("Forward"));
+    const backButton = document.createElement('button')
+    const forwardButton = document.createElement('button')
+    backButton.appendChild(document.createTextNode('Back'))
+    forwardButton.appendChild(document.createTextNode('Forward'))
     backButton.onclick = () => history.go(-1)
     forwardButton.onclick = () => history.go(1)
-    const firstDiv = document.querySelector(".my-div");
-    firstDiv.appendChild(backButton);
-    firstDiv.appendChild(forwardButton);
+    const firstDiv = document.querySelector('.my-div')
+    firstDiv.appendChild(backButton)
+    firstDiv.appendChild(forwardButton)
 }
 
 export const getBrowserInfo = () => {
-    const nav = window.navigator;
+    const nav = window.navigator
     console.log(
-       `Название браузера: ${nav.appCodeName}\n`,
-       `Выбранный язык: ${nav.language}\n`,
+        `Название браузера: ${nav.appCodeName}\n`,
+        `Выбранный язык: ${nav.language}\n`,
         `Доступные языки: ${nav.languages}\n`,
         `User agen object: ${nav.userAgent}`
     )
 }
-
-
-
