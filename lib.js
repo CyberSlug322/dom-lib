05e71aa3b8a532bacec5740b4d2099e1a17f0299
Cyber Slug <lasminski@mail.ru>
2022-03-01 21:11:56 +0300

    feat: add onresize func to lib.js

--- lib.js
+++ lib.js
@@ -1,1 +1,7 @@
 
+
+    window.onresize = function() {
+        WIDTH = window.innerWidth;
+        HEIGHT = window.innerHeight;
+        console.log(`Current window size is ${WIDTH}x${HEIGHT}`);
+      }
